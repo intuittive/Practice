285df0dcc21c3af9ebb719e3ea76b12f312922b8
intuittive <intuittive@protonmail.com>
2021-05-08 19:54:33 -0400

    Clean-up & refactor

--- StopWatch/js/app.js
+++ StopWatch/js/app.js
@@ -2,13 +2,9 @@ var hundredths = 0;
 var seconds = 0;
 var minutes = 0;
 var interval = null;
-var displayHundredths = "00";
-var displaySeconds = "00";
-var displayMinutes = "00";
 var status = "stopped;";
 
 var timerToggleButton = document.getElementById("start-stop");
-// var stopButton = document.getElementById("button-stop");
 var resetButton = document.getElementById("button-reset");
 
 var secondsEl = document.getElementById("seconds");
@@ -23,8 +19,11 @@ function timerToggleHandler() {
 function startTimer() {
   interval = setInterval(() => {
     incrementTime();
-    setTimerVals();
-    setTimerDisplay();
+    setTimerDisplay(
+      getTimerDisplayVal(hundredths),
+      getTimerDisplayVal(seconds),
+      getTimerDisplayVal(minutes)
+    );
   }, 1);
   setToggleButtonText("Stop");
 }
@@ -38,11 +37,8 @@ function stopTimer() {
 function resetTimer() {
   if (isTimerActive()) clearInterval(interval);
   interval = null;
-  displayHundredths = "00";
-  displaySeconds = "00";
-  displayMinutes = "00";
   setToggleButtonText("Start");
-  setTimerDisplay();
+  setTimerDisplay("00", "00", "00");
 }
 
 function incrementTime() {
@@ -60,22 +56,11 @@ function incrementTime() {
   }
 }
 
-function setTimerVals() {
-  displayHundredths = hundredths;
-  displaySeconds = seconds;
-  displayMinutes = minutes;
-  if (displayHundredths <= 9) {
-    displayHundredths = "0" + displayHundredths;
-  }
-  if (displaySeconds <= 9) {
-    displaySeconds = "0" + displaySeconds;
-  }
-  if (displayMinutes <= 9) {
-    displayMinutes = "0" + displayMinutes;
-  }
+function getTimerDisplayVal(timeVal) {
+  return timeVal <= 9 ? "0" + timeVal : timeVal;
 }
 
-function setTimerDisplay() {
+function setTimerDisplay(displayHundredths, displaySeconds, displayMinutes) {
   hundredthsEl.innerHTML = displayHundredths;
   secondsEl.innerHTML = displaySeconds;
   minutesEl.innerHTML = displayMinutes;
