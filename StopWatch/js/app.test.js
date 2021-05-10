9768f20f4a4e1b5dd890a22b940f60b716d43ac8
intuittive <intuittive@protonmail.com>
2021-05-10 17:36:59 -0400

    Start working on config for testing

---
+++ StopWatch/js/app.test.js
@@ -0,0 +1,11 @@
+import { expect, test } from "@jest/globals";
+// import { getTimerDisplayVal } from "./app";
+
+// test("returns equivalent string if number already has two digits", () => {
+//   const res = getTimerDisplayVal(10);
+//   expect(res).toBe("10");
+// });
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(3).toBe(3);
+});
